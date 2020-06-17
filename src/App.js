61eee96e948b0c646b106fd437603ff12c4c1515
Simon <ghdud0503@gmail.com>
2020-06-17 20:48:12 +0900

    setting-React!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Love({ fav, pic }) {
+  // 매개변수 안의 fav 키값을 지정
+  // 만약 매개변수가 props 일시 밑에 값은 {props.fav} 로 입력 가능
+  return (
+    <div>
+      <h2>I love {fav}</h2>
+      <img src={pic} alt={fav} />
+    </div>
+  );
+}
+
+const personILike = [
+  {
+    id: 1,
+    name: "Rick",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BYTRhMjhjMTEtZTY0MC00NjAyLWJkNDQtZmYzN2FjNDM1NTI0XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY1200_CR744,0,630,1200_AL_.jpg",
+  },
+  {
+    id: 2,
+    name: "Morty",
+    image: "https://static.tvtropes.org/pmwiki/pub/images/morty_smith_2.png",
+  },
+  {
+    id: 3,
+    name: "Summer",
+    image:
+      "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest/top-crop/width/360/height/360?cb=20160919183158",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React!</h1>
+      {personILike.map((human) => {
+        return <Love key={human.id} fav={human.name} pic={human.image} />;
+      })}
     </div>
   );
 }
