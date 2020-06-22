b48a61694f50ef2805c7367c8aafb7662da05cad
Simon <ghdud0503@gmail.com>
2020-06-22 22:49:33 +0900

    Updates

---
+++ precache-manifest.f80e12069fef574a563033dd8fb0e141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cf4d189412886aa27a9bafd7c78b98",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9ada1b2b37cad9c2de61",
+    "url": "/movie_app_2020/static/css/main.aed7a57e.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ada1b2b37cad9c2de61",
+    "url": "/movie_app_2020/static/js/main.bba15cda.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
